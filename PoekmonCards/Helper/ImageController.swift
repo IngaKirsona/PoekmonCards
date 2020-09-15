1e6ee1665c99c0fc9cf8c82e062a5eb3800754c7
IngaKirsona <ingas@inbox.lv>
2020-09-15 11:06:38 +0300

    1:02:26

---
+++ PoekmonCards/Helper/ImageController.swift
@@ -0,0 +1,29 @@
+//
+//  ImageController.swift
+//  PoekmonCards
+//
+//  Created by Inga Kirsona on 15/09/2020.
+//  Copyright © 2020 Inga Kirsona. All rights reserved.
+//
+
+import UIKit
+class ImageController {
+    static func getImage(for url: String, completion: @escaping (UIImage?) -> Void){
+//-------> to check if url is a String
+        guard let imageURL = URL(string: url) else{
+            fatalError("Could not create url from string: \(url)")
+        }
+        NetworkController.performRequest(for: imageURL, httpMethod: .Get) {
+            (data, error) in
+            guard let data = data, let image = UIImage(data: data) else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+                return
+            }
+            DispatchQueue.main.async {
+                completion(image)
+            }
+        }
+    }
+}
