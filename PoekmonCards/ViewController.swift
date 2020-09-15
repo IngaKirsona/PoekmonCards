11be9630b597559a101fe6ae8c021b6a6b212689
IngaKirsona <ingas@inbox.lv>
2020-09-15 09:36:54 +0300

    Initial Commit

---
+++ PoekmonCards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PoekmonCards
+//
+//  Created by Inga Kirsona on 15/09/2020.
+//  Copyright © 2020 Inga Kirsona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
