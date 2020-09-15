7eed057800bc70aeea9491c0b23a9919ee1f42ce
IngaKirsona <ingas@inbox.lv>
2020-09-15 11:40:37 +0300

    1:19:41

--- PoekmonCards/Controller/PokemonViewController.swift
+++ PoekmonCards/Controller/PokemonViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class PokemonViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var pokey: [Pokemon] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Pokemon List"
+//-------> the same as to connect with control+view controller and choose dataSource, delegate
+        tableView.dataSource = self
     }
+    
+    func getPokemon(){
+        let url = URL(string: "https://api.pokemontcg.io/v1/cards")!
+        
+        NetworkController.performRequest(for: url, httpMethod: .Get){
+            (data, error) in
+            if let error = error{
+                print("error getting from url \(url.absoluteString), err: \(error.localizedDescription)")
+            }
+            if let data = data{
+                do{
+                let card = try JSONDecoder().decode(Card.self, from: data)
+                    self.pokey = card.cards
+                }catch{
+                    print ("failed to decode pokemon from pokey data \(error.localizedDescription). Data \(data)")
+                }
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+        }
+    }
+    
 
-
+extension PokemonViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokey.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "pokeyCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
 }
-
